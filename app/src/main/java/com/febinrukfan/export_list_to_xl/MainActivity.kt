10f23fba2a58c66a3701d58748a6a9bf3cf7e6bd
Febin Rukfan <32138498+FebinRukfan@users.noreply.github.com>
2023-06-22 23:19:42 -0400

    ADDED sample data to RoomDB

--- app/src/main/java/com/febinrukfan/export_list_to_xl/MainActivity.kt
+++ app/src/main/java/com/febinrukfan/export_list_to_xl/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.febinrukfan.export_list_to_xl.application.GlobalSpace
+import com.febinrukfan.export_list_to_xl.data.model.Employee
 import com.febinrukfan.export_list_to_xl.databinding.ActivityMainBinding
 import com.febinrukfan.export_list_to_xl.presentation.adapter.EmployeeAdapter
 import com.febinrukfan.export_list_to_xl.presentation.viewmodel.MainActivityViewModel
@@ -20,23 +21,25 @@ class MainActivity : AppCompatActivity() {
     private lateinit var bindingObj: ActivityMainBinding
     private lateinit var empAdapter: EmployeeAdapter
 
+    private var newEmployee: Employee = Employee(null, "")
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
         bindingObj = DataBindingUtil.setContentView(this, R.layout.activity_main)
         (application as GlobalSpace).getAppComponentInstance().inject(this)
+
+
+
         mainActivityViewModel =
             ViewModelProvider(this, mainActivityViewModelFactory)[MainActivityViewModel::class.java]
-//        bindingObj.fabBtn.setOnClickListener {
-//            val intent = Intent(this@MainActivity, AddEmployeeActivity::class.java)
-//            getResult.launch(intent)
-//        }
-//        initialiseRecyclerVw()
-//        displayEmployeeData()
+
+        newEmployee = Employee(1,"Febin")
+        mainActivityViewModel.addEmployee(newEmployee)
+
+//        bindingObj.fabBtn.se
     }
 
 
